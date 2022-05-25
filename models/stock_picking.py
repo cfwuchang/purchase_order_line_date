95081fe44212867d6e3c12f9eda989218ca4f51e
cfwuchang <67139173+cfwuchang@users.noreply.github.com>
2022-05-25 18:07:44 +0800

    Add files via upload

---
+++ models/stock_picking.py
@@ -0,0 +1,10 @@
+from odoo import models, fields
+import datetime
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    def _compute_scheduled_date(self):
+        for picking in self:
+            picking.scheduled_date = datetime.date.today()
