c9631c50379a02c27981c839a1dcbb3967b3d4aa
cfwuchang <67139173+cfwuchang@users.noreply.github.com>
2022-07-01 11:23:45 +0800

    Update stock_picking.py

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -8,3 +8,4 @@ class StockPicking(models.Model):
     def _compute_scheduled_date(self):
         for picking in self:
             picking.scheduled_date = datetime.date.today()
+            picking.move_line_ids.date=datetime.date.today()
